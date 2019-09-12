ae5505e117b10822e972360937fb1cc9106eabb5
Aaron Jonk <2119311@talnet.nl>
2019-09-12 17:27:18 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,8 @@
+<?php
+echo "Hallo gebruiker!";
+echo " Wat is jouw voornaam? ";
+$naam = readline();
+echo "Wat is jouw achternaam? ";
+$achter = readline();
+echo "Naam -> ".$naam." ".$achter;
+?>
